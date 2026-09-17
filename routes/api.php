7a4fae35a685e71f963d6732e0115439eec313c3
Amirhossein_Shahbaz <a.shahbaz2001tkd@gmail.com>
2026-09-17 19:17:22 +0330

    Clean up api routes structure

--- routes/api.php
+++ routes/api.php
@@ -23,50 +23,45 @@ Route::prefix('auth')->group(function () {
 Route::prefix('profile')->middleware(['auth:api'])->group(function () {
     Route::post('/', [AuthController::class, 'profile']);
     Route::patch('update', [AuthController::class, 'update']);
-    Route::get('logout', [AuthController::class, 'logout']);
+    Route::post('logout', [AuthController::class, 'logout']);
 });
 
-Route::prefix('desk')->middleware(['auth:api', 'isAdmin:admin'])->group(function () {
+Route::prefix('desks')->middleware(['auth:api', 'isAdmin:admin'])->group(function () {
     Route::get('/', [DeskController::class, 'index']);
-    Route::post('/new', [DeskController::class, 'store']);
-    Route::patch('/update/{desk}', [DeskController::class, 'update']);
-    Route::delete('/delete/{desk}', [DeskController::class, 'destroy']);
+    Route::post('/', [DeskController::class, 'store']);
+    Route::patch('/{desk}', [DeskController::class, 'update']);
+    Route::delete('/{desk}', [DeskController::class, 'destroy']);
 });
 
-Route::prefix('category')->middleware(['auth:api', 'isAdmin:admin'])->group(function () {
+Route::prefix('categories')->middleware(['auth:api', 'isAdmin:admin'])->group(function () {
     Route::get('', [CategoryController::class, 'index']);
-    Route::post('/new', [CategoryController::class, 'store']);
-    Route::patch('/update/{category}', [CategoryController::class, 'update']);
-    Route::delete('/delete/{category}', [CategoryController::class, 'destroy']);
+    Route::post('/', [CategoryController::class, 'store']);
+    Route::patch('/{category}', [CategoryController::class, 'update']);
+    Route::delete('/{category}', [CategoryController::class, 'destroy']);
 });
 
-Route::prefix('menu')->group(function () {
+Route::prefix('menus')->group(function () {
     Route::get('/', [MenuItemController::class, 'index']);
-//    Route::get('/item', [MenuItemController::class, 'index']);
-//    Route::get('menu', [MenuItemController::class, 'index']);
     Route::middleware(['auth:api', 'isAdmin:admin'])->group(function () {
-        Route::post('/new-item', [MenuItemController::class, 'store']);
-        Route::post('/update/{menuItem}', [MenuItemController::class, 'update']);
-        Route::delete('/delete/{menuItem}', [MenuItemController::class, 'destroy']);
+        Route::post('/', [MenuItemController::class, 'store']);
+        Route::post('/{menuItem}', [MenuItemController::class, 'update']);
+        Route::delete('/{menuItem}', [MenuItemController::class, 'destroy']);
     });
 });
 
-Route::prefix('reservation')->middleware(['auth:api'])->group(function () {
+Route::prefix('reservations')->middleware(['auth:api'])->group(function () {
     Route::get('/', [ReservationController::class, 'index']);
-    Route::post('/new', [ReservationController::class, 'store']);
+    Route::post('/', [ReservationController::class, 'store']);
     Route::post('/{reservation}/confirmed', [ReservationController::class, 'confirmed'])->middleware(['checkPermission:confirm_reserve']);
     Route::post('/{reservation}/completed', [ReservationController::class, 'completed'])->middleware(['checkPermission:complete_reserve']);
     Route::post('/{reservation}/cancelled', [ReservationController::class, 'cancelled'])->middleware(['checkPermission:cancel_reserve']);
-//    Route::post('/update/{reservation}', [ReservationController::class, 'update']);
-//    Route::delete('/delete/{reservation}', [ReservationController::class, 'destroy']);
     Route::middleware(['isAdmin:admin'])->group(function () {
-//        Route::get('topDesksByRevenue', [ReservationController::class, 'report_best_desk']);
     });
 });
 
-Route::prefix('order')->middleware(['auth:api'])->group(function () {
+Route::prefix('orders')->middleware(['auth:api'])->group(function () {
     Route::get('/', [OrderController::class, 'index']);
-    Route::post('/new', [OrderController::class, 'store']);
+    Route::post('/', [OrderController::class, 'store']);
     Route::post('/{order}/served', [OrderController::class, 'serve'])->middleware(['checkPermission:serve_order']);
     Route::post('/{order}/paid', [OrderController::class, 'pay'])->middleware(['checkPermission:pay_order']);
     Route::post('/{order}/cancelled', [OrderController::class, 'cancel'])->middleware(['checkPermission:cancel_order']);
@@ -75,8 +70,8 @@ Route::prefix('order')->middleware(['auth:api'])->group(function () {
     });
 });
 
-Route::prefix('report')->middleware(['auth:api', 'isAdmin:admin'])->group(function () {
-    Route::get('topDesks', [ReportController::class, 'report_best_desks']);
-    Route::get('topMenuItems', [ReportController::class, 'report_best_items']);
+Route::prefix('reports')->middleware(['auth:api', 'isAdmin:admin'])->group(function () {
+    Route::post('topDesks', [ReportController::class, 'report_best_desks']);
+    Route::post('topMenuItems', [ReportController::class, 'report_best_items']);
 });
 
